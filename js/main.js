787379586e9de86e99e48a7184edd27452bb2f10
hoarh <hoarh@hannahs-MacBook-Pro-2.local>
2015-07-13 14:41:51 -0400

    Made folder

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+  // Handler for .ready() called.
+
+//detect click
+
+//on click, get user input
+
+//
+
+//convert user input (f) to celsius (c)
+
+//display celsius result
+
+});
