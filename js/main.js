ad65598e47385532ceb14da8052ce510841689bd
hoarh <hoarh@hannahs-MacBook-Pro-2.local>
2015-07-13 15:57:09 -0400

    completed JS and styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(function() {
-  // Handler for .ready() called.
-
+// Handler for .ready() called.
+	$('.convert').click(tempConvert);
 //detect click
-
+	function tempConvert () {
 //on click, get user input
+	var userTemp = $('.userTemp').val();
+	userTemp = parseInt(userTemp);
+	
+	var newTemp = ((userTemp-32)*(5/9));
+	console.log('Aha! '+userTemp+' F is '+newTemp+' C!');
 
-//
+	$('.result').html(newTemp);
+}
 
 //convert user input (f) to celsius (c)
 
